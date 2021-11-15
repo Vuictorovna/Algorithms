18aca2a0da68805fea1116e78d1917f901d3dea1
Volha Sakharevich <vuictorovna@gmail.com>
2021-11-15 22:45:04 +0000

    minor changes

--- yandex_contest/binary_sum.py
+++ yandex_contest/binary_sum.py
@@ -1,6 +1,5 @@
 def convert_to_list(string):
-    list_x = [int(i) for i in string]
-    return list_x
+    return [int(i) for i in string]
 
 
 def main():
@@ -10,25 +9,23 @@ def main():
     num_2 = input()
     list_2 = convert_to_list(num_2)
 
-    reversed_num_1 = list(reversed(list_1))
-    reversed_num_2 = list(reversed(list_2))
+    list_1 = list(reversed(list_1))
+    list_2 = list(reversed(list_2))
 
-    if len(reversed_num_1) > len(reversed_num_2):
-        temp = reversed_num_1
-        reversed_num_1 = reversed_num_2
-        reversed_num_2 = temp
+    if len(list_1) > len(list_2):
+        list_1, list_2 = list_2, list_1
 
-    for _ in range(len(reversed_num_2) - len(reversed_num_1)):
-        reversed_num_1.append(0)
+    for _ in range(len(list_2) - len(list_1)):
+        list_1.append(0)
 
     result = []
     summa = 0
     rest = 0
 
-    for i in range(len(reversed_num_1)):
-        summa = reversed_num_1[i] + reversed_num_2[i] + rest
+    for i in range(len(list_1)):
+        summa = list_1[i] + list_2[i] + rest
         rest = summa // 2
-        summa = summa % 2
+        summa %= 2
         result.append(summa)
 
     if rest > 0:
