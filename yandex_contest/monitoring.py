509600fa78cb4bb355edcf1009f32459fda76ec2
Volha Sakharevich <vuictorovna@gmail.com>
2021-11-24 18:14:48 +0000

    changed name of rows and colums variables; removed None from def main()

--- yandex_contest/monitoring.py
+++ yandex_contest/monitoring.py
@@ -1,16 +1,16 @@
 def main():
-    rows_number = int(input())
-    colums_number = int(input())
-    if rows_number == 0 or colums_number == 0:
-        return None
-    matrix = [input().split() for _ in range(rows_number)]
+    rows_count = int(input())
+    colums_count = int(input())
+    if rows_count == 0 or colums_count == 0:
+        return
+    matrix = [input().split() for _ in range(rows_count)]
 
     new_matrix = []
-    for j in range(colums_number):
-        for i in range(rows_number):
+    for j in range(colums_count):
+        for i in range(rows_count):
             new_matrix.append(matrix[i][j])
 
-    sliced_new_matrix = [new_matrix[i:i+rows_number] for i in range(0, len(new_matrix), rows_number)]
+    sliced_new_matrix = [new_matrix[i:i+rows_count] for i in range(0, len(new_matrix), rows_count)]
     for slice in sliced_new_matrix:
         result = ' '.join(str(k) for k in slice)
         print(result)
